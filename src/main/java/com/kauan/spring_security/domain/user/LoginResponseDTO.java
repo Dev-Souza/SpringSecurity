1fc37e6cef8701a57030deba2390f33bec70dae3
Dev-Souza <kauan.souza.dev@gmail.com>
2024-10-08 13:21:57 -0300

    Subindo meu aprendizado de Spring Security com roles de admin e usuário padrão

---
+++ src/main/java/com/kauan/spring_security/domain/user/LoginResponseDTO.java
@@ -0,0 +1,5 @@
+package com.kauan.spring_security.domain.user;
+
+public record LoginResponseDTO(String token) {
+
+}
---
+++ src/main/java/com/kauan/spring_security/domain/user/RegisterDTO.java
@@ -0,0 +1,5 @@
+package com.kauan.spring_security.domain.user;
+
+public record RegisterDTO(String login, String password, UserRole role){
+
+}
